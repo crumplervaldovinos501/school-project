45e3f09d72ddf2a0dc80971f3cfdd1eafd03b382
crumplervaldovinos501 <crumplervaldovinos501@gmail.com>
2025-03-11 00:14:20 +0000

    Add 5.go

---
+++ src/5.go
@@ -0,0 +1,13 @@
+  // Package main will be used to hold our application
+package main
+
+import "fmt"
+
+// Function main is the entry point of the program
+func main() {
+	// Declare a variable named number and assign it an initial value of 42
+	number := 42
+
+	// Print the value of the variable to the console
+	fmt.Println("The number is:", number)
+}
