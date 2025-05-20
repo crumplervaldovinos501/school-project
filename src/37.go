fe324a47a92f7f443962974b5f5d85704e6f6e03
crumplervaldovinos501 <crumplervaldovinos501@gmail.com>
2025-05-20 00:33:41 +0000

    Add 37.go

---
+++ src/37.go
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    if n <= 0:
+        return "Input should be a positive integer."
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+
+    sequence = [0, 1]
+    while len(sequence) < n:
+        sequence.append(sequence[-1] + sequence[-2])
+
+    return sequence
